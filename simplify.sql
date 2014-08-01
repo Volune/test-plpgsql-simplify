f0fd8d2f3e9ca51bebd524c1b598a70ab320c395
Jeremy Judeaux <jjx@f4-group.com>
2014-08-01 15:36:04 +0200

    working solution

--- simplify.sql
+++ simplify.sql
@@ -3,18 +3,27 @@ CREATE OR REPLACE FUNCTION simplify_geometry(geom geometry, tolerance float)
 $BODY$
 
 declare linestring geometry;
+declare simplified geometry;
+declare testSegment geometry;
 
 begin
 
-  --get linestring from original polyon's outer ring
-  linestring := st_exteriorring(geom);
-  --add a new repeated point after start point
-  linestring := st_addpoint(linestring, st_pointn(linestring, ST_NPoints(linestring)-1), 0);
-  linestring := st_simplify(linestring, $2);
-  linestring := st_removepoint(linestring, 0);
-  linestring := st_removepoint(linestring, ST_NPoints(linestring)-1);
-  linestring := st_addpoint(linestring, ST_StartPoint(linestring));
+  simplified := st_simplify(geom, $2);
 
-  return st_makepolygon(linestring);
+  if ST_IsClosed(simplified) then
+    linestring := st_exteriorring(simplified);
+    testSegment := ST_MakeLine(ST_PointN(linestring, 2), ST_PointN(linestring, ST_NPoints(linestring)-1));
+    raise notice 'test point : % ', st_astext(ST_StartPoint(linestring));
+    raise notice 'test line : % ', st_astext(testSegment);
+    raise notice 'distance : % ', ST_Distance(ST_StartPoint(linestring), testSegment);
+    if ST_Distance(ST_StartPoint(linestring), testSegment) < tolerance then
+      linestring := st_removepoint(linestring, 0);
+      linestring := st_removepoint(linestring, ST_NPoints(linestring)-1);
+      linestring := st_addpoint(linestring, ST_StartPoint(linestring));
+      return st_makepolygon(linestring);
+    end if;
+  end if;
+  return simplified;
+
 end
 $BODY$  LANGUAGE plpgsql IMMUTABLE
